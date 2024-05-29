42e02c899b9f0e1ca7b4fe81862e37c2aadc5bdb
José Augusto <josemoura212@gmail.com>
2024-05-29 18:36:59 -0300

    create script with create container postgres and verify

---
+++ launch_postgres.ps1
@@ -0,0 +1,55 @@
+if (!(Get-Command psql -ErrorAction SilentlyContinue)) {
+    Write-Host "Error: psql is not installed."
+    exit 1
+}
+
+# Check if sqlx is installed
+if (!(Get-Command sqlx -ErrorAction SilentlyContinue)) {
+    Write-Host "Error: sqlx is not installed."
+    Write-Host "Use:"
+    Write-Host " cargo install --version=0.5.7 sqlx-cli --no-default-features --features native-tls,postgres"
+    Write-Host "to install it."
+    exit 1
+}
+
+# Check if a custom user has been set, otherwise default to 'postgres'
+$DB_USER = if ($env:POSTGRES_USER) { $env:POSTGRES_USER } else { 'postgres' }
+# Check if a custom password has been set, otherwise default to 'password'
+$DB_PASSWORD = if ($env:POSTGRES_PASSWORD) { $env:POSTGRES_PASSWORD } else { 'password' }
+# Check if a custom database name has been set, otherwise default to 'newsletter'
+$DB_NAME = if ($env:POSTGRES_DB) { $env:POSTGRES_DB } else { 'newsletter' }
+# Check if a custom port has been set, otherwise default to '5432'
+$DB_PORT = if ($env:POSTGRES_PORT) { $env:POSTGRES_PORT } else { '5432' }
+
+# Define the volume path on the host
+$DB_VOLUME_PATH = "C:/path/to/your/volume"
+
+$env:DATABASE_URL = "postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}"
+
+# Launch postgres using Docker
+docker run `
+--name postgres_zero2prod `
+-e POSTGRES_USER=$DB_USER `
+-e POSTGRES_PASSWORD=$DB_PASSWORD `
+-e POSTGRES_DB=$DB_NAME `
+-p "${DB_PORT}:5432" `
+-v "postgres_zero2prod:/var/lib/postgresql/data" `
+-d postgres `
+postgres -N 1000
+
+# Keep pinging Postgres until it's ready to accept commands
+# $env:PGPASSWORD = "${DB_PASSWORD}"
+# do {
+#     try {
+#         psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'
+#         $connected = $true
+#     } catch {
+#         Write-Host "Postgres is still unavailable - sleeping"
+#         Start-Sleep -Seconds 1
+#     }
+# } until ($connected)
+
+# Write-Host "Postgres is up and running on port ${DB_PORT}!"
+
+# sqlx database create
+sqlx migrate add create_subscriptions_table
