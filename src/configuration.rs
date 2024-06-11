b81f260203d9d191b1e688a2ef46ac7741fb9424
José Augusto <josemoura212@gmail.com>
2024-06-11 13:19:51 -0300

    chore: Update database configuration for local and production environments

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,7 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
@@ -9,6 +12,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -17,30 +21,30 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        )
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
     }
-    pub fn connection_string_without_db(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        )
+    pub fn with_db(&self) -> PgConnectOptions {
+        let options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace)
     }
 }
 
@@ -77,6 +81,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     settings = settings.add_source(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     );
+
+    settings = settings.add_source(config::Environment::with_prefix("app").separator("__"));
 
     let settings = settings.build()?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -11,13 +11,11 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
-        .expect("Failed to connect to Postgres.");
+    //todo - Old way of connecting to Postgres
+    // let connection_pool = PgPool::connect_lazy(&configuration.database.with_db())
+    //     .expect("Failed to connect to Postgres.");
 
-    //todo - implements error
-    // let connection_pool = PgPoolOptions::new()
-    //     .connect_timeout(std::time::Duration::from_secs(2))
-    //     .connect_lazy(&configuration.database.connection_string());
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
