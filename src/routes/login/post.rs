1fafe0df4d7d82472d59c582cff380847c6ef7f5
José Augusto <josemoura212@gmail.com>
2024-06-26 17:23:21 -0300

    10.6.4

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,15 @@
+use crate::authentication::AuthError;
+use crate::routes::error_chain_fmt;
 use actix_web::http::header::LOCATION;
+use actix_web::http::StatusCode;
 use actix_web::web;
 use actix_web::HttpResponse;
+use actix_web::ResponseError;
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::authentication::validate_credentials;
+use crate::authentication::Credentials;
 
 #[allow(unused)]
 #[derive(serde::Deserialize)]
@@ -10,8 +18,52 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+#[tracing::instrument(
+skip(form, pool),
+fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            Ok(HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish())
+        }
+        Err(e) => match e {
+            AuthError::InvalidCredentials(_) => Err(LoginError::AuthError(e.into())),
+            _ => Err(LoginError::UnexpectedError(e.into())),
+        },
+    }
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        }
+    }
 }
