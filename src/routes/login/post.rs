c82c8e7f8ff888a55c1b2b5c97ef4a000a2057c9
José Augusto <josemoura212@gmail.com>
2024-06-27 20:19:14 -0300

    10.6.4.7

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,11 +1,14 @@
 use crate::authentication::AuthError;
 use crate::routes::error_chain_fmt;
+use crate::startup::HmacSecret;
+use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::http::StatusCode;
 use actix_web::web;
 use actix_web::HttpResponse;
 use actix_web::ResponseError;
-use secrecy::Secret;
+use hmac::{Hmac, Mac};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
 use crate::authentication::validate_credentials;
@@ -19,13 +22,14 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-skip(form, pool),
+skip(form, pool,secret),
 fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-) -> Result<HttpResponse, LoginError> {
+    secret: web::Data<HmacSecret>,
+) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
@@ -38,10 +42,28 @@ pub async fn login(
                 .insert_header((LOCATION, "/"))
                 .finish())
         }
-        Err(e) => match e {
-            AuthError::InvalidCredentials(_) => Err(LoginError::AuthError(e.into())),
-            _ => Err(LoginError::UnexpectedError(e.into())),
-        },
+        Err(e) => {
+            let e = match e {
+                AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+                AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+            };
+
+            let query_string = format!("error={}", urlencoding::Encoded::new(e.to_string()));
+            let hmac_tag = {
+                let mut mac =
+                    Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
+                        .unwrap();
+                mac.update(query_string.as_bytes());
+                mac.finalize().into_bytes()
+            };
+            let response = HttpResponse::SeeOther()
+                .insert_header((
+                    LOCATION,
+                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
+                ))
+                .finish();
+            Err(InternalError::from_response(e, response))
+        }
     }
 }
 
@@ -60,10 +82,22 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    // fn error_response(&self) -> HttpResponse {
+    //     let query_string = format!("error={}", urlencoding::Encoded::new(self.to_string()));
+    //     let secret: &[u8] = todo!();
+    //     let hmac_tag = {
+    //         let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret).unwrap();
+    //         mac.update(query_string.as_bytes());
+    //         mac.finalize().into_bytes()
+    //     };
+    //     HttpResponse::build(self.status_code())
+    //         .insert_header((
+    //             LOCATION,
+    //             format!("/login?{}&tag={:x}", query_string, hmac_tag),
+    //         ))
+    //         .finish()
+    // }
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
+        StatusCode::SEE_OTHER
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -5,6 +5,7 @@ use crate::routes::{
 };
 use actix_web::web::Data;
 use actix_web::{dev::Server, web, App, HttpServer};
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -43,6 +44,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -68,6 +70,7 @@ fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -75,19 +78,23 @@ fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
-            .route("/login", web::get().to(login_form))
-            .route("/login", web::post().to(login))
-            .route("/", web::get().to(home))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
